2b17a2df842b7e4dcf54e866ee853e1c24cc0048
波 <450951373@qq.com>
2021-08-20 03:37:20 +0800

    添加项目文件。

---
+++ DivControlComponent/DivComponent/AfTextBox.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace DivControlComponent.DivComponent
+{
+    partial class AfTextBox
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Edit = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Edit
+            // 
+            this.Edit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.Edit.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Edit.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Edit.Location = new System.Drawing.Point(-1, 12);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(279, 16);
+            this.Edit.TabIndex = 0;
+            // 
+            // AfTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.Edit);
+            this.Name = "AfTextBox";
+            this.Size = new System.Drawing.Size(279, 43);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Edit;
+    }
+}
---
+++ DivControlComponent/DivComponent/AfTextBox.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DivControlComponent.DivComponent
+{
+    public partial class AfTextBox : UserControl
+    {
+        public AfTextBox()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLayout(LayoutEventArgs e)
+        {
+            base.OnLayout(e);
+
+            if (this.Controls.Count == 0) return;
+            Control c = this.Controls[0];
+
+            Padding p = this.Padding;
+            int x = 0, y = 0;
+            int w = this.Width, h = this.Height;
+            w -= (p.Left + p.Right);
+            x += p.Left;
+            int h2 = c.PreferredSize.Height;
+            if (h2 > h) h2 = h;
+            y = (h - h2) / 2;
+            c.Location = new Point(x, y);
+            c.Size = new Size(w,h2);
+        }
+
+        //private void InitializeComponent()
+        //{
+        //    this.Edit = new System.Windows.Forms.TextBox();
+        //    this.SuspendLayout();
+
+        //    this.Edit.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        //    this.Edit.Font = new System.Drawing.Font("宋体", 10.5f, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+        //    this.Edit.Location = new System.Drawing.Point(43,25);
+        //    this.Edit.Name = "Edit";
+        //    this.Edit.Size = new System.Drawing.Size(143,16);
+        //    this.Edit.TabIndex = 0;
+
+        //    this.BackColor = System.Drawing.Color.White;
+        //    this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        //    this.Controls.Add(this.Edit);
+
+        //    this.Name = "AfTextBox";
+        //    this.Size = new System.Drawing.Size(238,76);
+        //    this.ResumeLayout(false);
+        //    this.PerformLayout();
+        //}
+
+        //public override string Text {
+        //    get { return this.Edit.Text; }
+        //    set { this.Text = value; }
+        //}
+    }
+}
